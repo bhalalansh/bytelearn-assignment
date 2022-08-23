75b461470b90524e0166aef58df234c36829eb8f
Ansh Bhalala <anshb@duck.com>
2022-08-24 00:41:37 +0530

    Added prometheus /metrics endpoint

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import os
+import logging
 from datetime import datetime
 from waitress import serve
+from prometheus_flask_exporter import PrometheusMetrics
 
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info("flask_heroku_app", "an assignment for bytelearn", version="0.0.1")
 
 @app.route("/")
 def home_route():
@@ -18,4 +24,4 @@ if __name__ == "__main__":
     if f"{os.environ.get('PROD')}".lower() == "true":
         serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     else:
-        app.run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)), debug=True)
+        app.run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)))
